a1857847a4ca88dbb8714ccd45fc3b5079a8718b
Abrom-code <gmikael1908@gmail.com>
2025-01-24 01:00:43 +0300

    Update App.js

--- week two/task2/src/App.js
+++ week two/task2/src/App.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>State</h1>
+      <h1>COUNTER</h1>
+      <p>{count}</p>
+      <button onClick={() => setCount(count+1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(0)} disabled = {count === 0}>RESET</button>
       
     </div>
   );
