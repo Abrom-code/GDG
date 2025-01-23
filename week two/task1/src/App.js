9876c056816eb94fcfc4e1618848bd34011455e4
Abrom-code <gmikael1908@gmail.com>
2025-01-24 00:14:02 +0300

    basic html

--- week two/task1/src/App.js
+++ week two/task1/src/App.js
@@ -1,1 +1,32 @@
+const users = [
+  { name: 'John', age: 25, email: 'ex@mail' },
+  { name: 'Jane', age: 24, email: 'ex@mail' },
+  { name: 'Bob', age: 30, email: 'ex@mail' },
+  { name: 'Alice', age: 29, email: 'ex@mail' }
+];
 
+export default function App(){
+  return(
+    <div>
+      <header>
+        <h1>Users</h1>
+        <main>
+          <UserCard {...users[0]}/>
+          <UserCard {...users[1]}/>
+          <UserCard {...users[2]}/>
+          <UserCard {...users[3]}/>
+
+        </main>
+      </header>
+    </div>
+  )
+}
+function UserCard({name, age, email}){
+  return(
+    <div className="user-card">
+      <h2>NAME : {name}</h2>
+      <p>AGE : {age}</p>
+      <p>EMAIL : {email}</p>
+    </div>
+  )
+}
